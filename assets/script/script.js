c867686e94d66da278feb5604efd9b81189c65fe
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-23 15:18:01 +1000

    update portfolio

--- assets/script/script.js
+++
@@ -1,8 +0,0 @@
-let projectOne = document.getElementById("project1");
-
-projectOne.addEventListener('mouseover', function() {
-  this.src = './assets/img/firstProject.png';
-});
-projectOne.addEventListener('mouseout', function() {
-  this.src = './assets/img/firstProjectLogo.png';
-});
